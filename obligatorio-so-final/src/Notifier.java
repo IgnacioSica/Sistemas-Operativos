687fd84b984c4d63d38ad2ccd67eded562c4c514
f-mehues <fmehues@somniosoftware.com>
2021-06-27 14:15:38 -0300

    Notifier implemented

--- obligatorio-so-final/src/Notifier.java
+++ obligatorio-so-final/src/Notifier.java
@@ -1,4 +1,30 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Notifier extends Thread{
+
+    private void writeNotification(String requestId, String centerId, String dosis)
+    {
+        FileWriter fileWriter = null;
+        PrintWriter pw = null;
+        try
+        {
+            fileWriter = new FileWriter("src/Usuarios agendados.txt", true);
+            pw = new PrintWriter(fileWriter);
+            pw.println("usuario: " + requestId + ", centro: "+ centerId + ", dosis: " + dosis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Close File
+           if (null != fileWriter){
+            fileWriter.close();
+           }
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+    }
 
     Notifier(Request request, String centerName, String dosis){
         this.request = request;
@@ -12,7 +38,7 @@ public class Notifier extends Thread{
     @Override
     public void run(){
         try{
-            NotificationWriter.WriteNotification(request.cedula, centerName, dosis);
+            writeNotification(request.cedula, centerName, dosis);
             System.out.println("            Se agendo la vacunacion del usuario " + request.cedula + " en el " + centerName + " en el nivel " + request.priorityLevel + " con un puntaje de " + request.priorityScore + " "+ dosis);
         }catch(Exception e){
 
